29471086a4972571a4be4739139777ff0a6da1dc
Benedict Mwanga <mwangaben@gmail.com>
2018-09-02 11:33:39 +0300

    Compouted 101

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('Computed', require('./components/ComputedProperties.vue'));
 
 const app = new Vue({
     el: '#app'
